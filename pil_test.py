23b1697a595821b883327b041a91462b1fc2aff7
hoyoung <hoyoung@sina.cn>
2015-10-14 22:45:24 +0800

    f

--- pil_test.py
+++ pil_test.py
@@ -1,6 +1,19 @@
 #coding=utf-8
 __author__ = 'Administrator'
+a = "abc"
+b = "cba"
 
-import sys
-sys.path.append("E:\\huyang\\Python\\site-packages\\PIL")
+print list(a)
+print list(b)
 
+def func(s,t):
+    print (lambda x:(x.sort(),x)[1])(list(s))
+    print list(s).sort()
+    s = "".join((lambda x:(x.sort(),x)[1])(list(s)))
+    t = "".join((lambda y:(y.sort(),y)[1])(list(t)))
+    if s == t:
+        return True
+    else:
+        return False
+
+print(func(a,b))
